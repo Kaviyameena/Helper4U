d308ee4a7e9b1ff83fc1df90c6215f0453237d10
Kaviyameena <131577198+Kaviyameena@users.noreply.github.com>
2023-11-27 20:36:37 +0530

    Add files via upload

---
+++ routes/todosRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { createDB, createTable, createList, showTodos, singleTodo, updateTodo, deleteSingleTodo } = require('../controllers/todosController');
+const router = express.Router();
+
+
+
+router.get('/create/database', createDB);
+router.get('/create/table', createTable);
+router.post('/create/list', createList);
+router.get('/show/todos', showTodos);
+router.get('/todo/:id', singleTodo);
+router.put('/update/todo/:id', updateTodo);
+router.delete('/delete/todo/:id', deleteSingleTodo);
+
+
+
+module.exports = router;
